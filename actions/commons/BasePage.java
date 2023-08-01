3c01bcd5054d8f7ce3477417981a86fc07e212c2
truong <truong922946@gmail.com>
2023-08-01 21:20:03 +0700

    Add Web browser function in class BasePage

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,4 +1,106 @@
 package commons;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+
 public class BasePage {
+
+    /*
+    1- Access Modifier : /public/ protected/ default/ private
+    2- Kieu du lieu cua ham : void/ int/ String/ Alert/ boolean/ Webdriver / WebElement
+    3- Ten Ham: co nghia theo chuc nang can viet : Tuan thu theo tieu chuan cua laptrinh
+    4- Co tham so hay ko (tuy vao chuc nang)
+    5- Kieu du lieu tra ve cho ham
+      + Neu co return du lieu tra ve se khop voi  kieu du lieu o so 2
+      + Neu nhu co return thi no la step cuoi cung
+     */
+
+    /* Web Browser function*/
+
+    public void openPageUrl(WebDriver driver, String urlPage) {
+        driver.get(urlPage);
+    }
+
+    public String getPageTitle(WebDriver driver) {
+        return driver.getTitle();
+    }
+
+    public String getCurrentPageUrl(WebDriver driver) {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageResource(WebDriver driver) {
+        return driver.getPageSource();
+    }
+
+    public void backToPage(WebDriver driver) {
+        driver.navigate().back();
+    }
+
+    public void forwardToPage(WebDriver driver) {
+        driver.navigate().forward();
+    }
+
+    public void refreshPageCurrent(WebDriver driver) {
+        driver.navigate().refresh();
+    }
+
+    public Alert waitAlertPresence(WebDriver driver) {
+        return new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
+    }
+
+    public void acceptToAlert(WebDriver driver) {
+        waitAlertPresence(driver).accept();
+    }
+
+    public void cancelToAlert(WebDriver driver) {
+        waitAlertPresence(driver).dismiss();
+    }
+
+    public String getTextAlert(WebDriver driver) {
+        return waitAlertPresence(driver).getText();
+    }
+
+    public void sendkeyToAlert(WebDriver driver, String valueToSend) {
+        waitAlertPresence(driver).sendKeys(valueToSend);
+    }
+
+    public void switchToWindowByID(WebDriver driver, String parentID) {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String runWindow : allWindows) {
+            if (!runWindow.equals(parentID)) {
+                driver.switchTo().window(runWindow);
+                break;
+            }
+        }
+    }
+
+    public void switchToWindowByTitle(WebDriver driver, String title) {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String runWindows : allWindows) {
+            driver.switchTo().window(runWindows);
+            String currentWin = driver.getTitle();
+            if (currentWin.equals(title)) {
+                break;
+            }
+        }
+    }
+
+    public void closeAllWindowsWithoutParent(WebDriver driver, String parentID) {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String runWindows : allWindows) {
+            if (!runWindows.equals(parentID)) {
+                driver.switchTo().window(runWindows);
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentID);
+    }
+
+
 }
